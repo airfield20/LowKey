2e3771e28922562e064a43108c39ab235144959a
Aaron C <afcofiel@umich.edu>
2019-04-19 15:00:32 -0400

    Added dummy

---
+++ app/src/main/java/com/example/lowkey/Dummy.java
@@ -0,0 +1,13 @@
+package com.example.lowkey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dummy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
